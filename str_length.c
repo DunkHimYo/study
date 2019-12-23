2edfe9b2358118a820fac9471e450854d5f31a04
dohyunkim123 <57661649+dohyunkim123@users.noreply.github.com>
2019-12-24 00:07:47 +0900

    Update str_length.c

--- str_length.c
+++ str_length.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
-size_t length(const char* __restrict string);
+size_t length(const char* __restrict string);//문자열 길이측정 함수
 
 int main()
 {
 	char str[] = "dohyunKim";
-	printf("%zd", length(str));
+	printf("%zd", length(str));//9출력
 }
 
 
 size_t length(const char* __restrict string)
 {
-	size_t count = 0;
-	while (*string++) ++count;
-	return count;
+	size_t count = 0;//카운트 변수
+	while (*string++) ++count;//카운트 루프
+	return count;카운트 리턴
 }
