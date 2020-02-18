e467b5ddb9839f240f08d03759a38bb37845a0a0
DunkHimYo <tjqn104@naver.com>
2020-02-18 23:24:42 +0900

    study

---
+++ python/function.py
@@ -0,0 +1,8 @@
+def calc():
+    a = 3
+    b = 5
+    def mul_add(x):
+        return a * x + b    # 함수 바깥쪽에 있는 지역 변수 a, b를 사용하여 계산
+    return mul_add          # mul_add 함수를 반환
+ 
+print(calc()(1))
