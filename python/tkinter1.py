8ae2f1d04444e8062c4e703e8f4269d5898e6156
DunkHimYo <tjqn104@naver.com>
2020-03-22 00:26:27 +0900

    study

---
+++ python/tkinter1.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window=Tk()
+window.title("do hyun")
+window.geometry("640x400+100+100")
+window.resizable(True,True)
+
+label=Label(window,{'text':'안녕하세요'})
+label.pack()
+
+window.mainloop()
