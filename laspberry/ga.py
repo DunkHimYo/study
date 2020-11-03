889156247e4a5061ddad4e1206a30ba33c3118d8
DunkHimYo <57661649+DunkHimYo@users.noreply.github.com>
2020-11-03 15:20:39 +0900

    Create ga.py

---
+++ laspberry/ga.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 라이브러리 로딩 ----------------------------------------------------------
+import spidev # SPI Module
+import time
+# 핀번호 상수 선언 --------------------------------------------------------
+SPI_CE = 1 # MCP3008연결된 CE번호
+LIGHT_CHANNEL = 0
+# 엔트리 포인트함수선언-----------------------------------------------------
+def main(args):
+ # 초기화
+    spi=spidev.SpiDev()
+    spi.open(0,SPI_CE)
+    spi.max_speed_hz=1000000 # 1MHz
+
+ # 기능구현
+    try:
+        while True:
+            adc=spi.xfer2([1, (8+LIGHT_CHANNEL) <<4, 0])
+            val=((adc[1]&0x03)<<8)+adc[2]
+            print( "Light is {0}Lux".format(int(val))) 
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        spi.close() # 리소스 시스템에 반환
+
+    return 0
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+
