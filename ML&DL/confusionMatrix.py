5ff5d2e8e743745c272faebd7414b62316db7bc6
DunkHimYo <57661649+DunkHimYo@users.noreply.github.com>
2021-02-10 20:20:35 +0900

    Update confusionMatrix.py

--- ML&DL/confusionMatrix.py
+++ ML&DL/confusionMatrix.py
@@ -1,9 +1,12 @@
 import numpy as np
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score, f1_score, accuracy_score
 
 y_true = [0, 0, 0, 1, 1, 1]
 y_pred = [1, 0, 0, 1, 1, 1]
 
 confMatrix = confusion_matrix(y_true, y_pred)
 print(confMatrix)
-print(np.trace(confMatrix)/sum(confMatrix.flat))
+print(accuracy_score(y_true, y_pred))
+print(recall_score(y_true, y_pred))
+print(f1_score(y_true, y_pred))
